ddded4ccef84025b05b04bba2851a0adf7c1e2c1
lditzel94 <lucianoditzel94@gmail.com>
2022-11-01 10:01:30 -0300

    table changed to plural format colum names has been added

--- src/main/java/com/alkemy/wallet/model/User.java
+++ src/main/java/com/alkemy/wallet/model/User.java
@@ -4,23 +4,24 @@ import javax.persistence.*;
 import java.sql.Timestamp;
 
 @Entity
-@Table( name = "USER" )
+@Table( name = "USERS" )
 public class User {
 
     @Id
     @GeneratedValue( strategy = GenerationType.IDENTITY )
+    @Column( name = "USER_ID" )
     private Integer userId;
 
-    @Column( nullable = false )
+    @Column( nullable = false, name = "FIRST_NAME" )
     private String firstName;
 
-    @Column( nullable = false )
+    @Column( nullable = false, name = "LAST_NAME" )
     private String lastName;
 
-    @Column( nullable = false, unique = true )
+    @Column( nullable = false, unique = true, name = "EMAIL" )
     private String email;
 
-    @Column( nullable = false )
+    @Column( nullable = false, name = "PASSWORD" )
     private String password;
 
     // TODO: check timestamps format and auditing

e00b51b531ddfb08fa910046c8570c706a93c16d
4nR3 <4nR34lvarez@gmail.com>
2022-11-01 17:52:22 -0300

    Added JWT and configuration

--- src/main/java/com/alkemy/wallet/model/User.java
+++ src/main/java/com/alkemy/wallet/model/User.java
@@ -1,11 +1,15 @@
 package com.alkemy.wallet.model;
 
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
 import javax.persistence.*;
 import java.sql.Timestamp;
+import java.util.Collection;
 
 @Entity
 @Table( name = "USERS" )
-public class User {
+public class User implements UserDetails {
 
     @Id
     @GeneratedValue( strategy = GenerationType.IDENTITY )
@@ -30,4 +34,107 @@ public class User {
 
     // TODO: check soft delete rules
     private Boolean softDelete;
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public User setUserId(Integer userId) {
+        this.userId = userId;
+        return this;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public User setFirstName(String firstName) {
+        this.firstName = firstName;
+        return this;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public User setLastName(String lastName) {
+        this.lastName = lastName;
+        return this;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public User setEmail(String email) {
+        this.email = email;
+        return this;
+    }
+
+    public User setPassword(String password) {
+        this.password = password;
+        return this;
+    }
+
+    public Timestamp getCreationDate() {
+        return creationDate;
+    }
+
+    public User setCreationDate(Timestamp creationDate) {
+        this.creationDate = creationDate;
+        return this;
+    }
+
+    public Timestamp getUpdateDate() {
+        return updateDate;
+    }
+
+    public User setUpdateDate(Timestamp updateDate) {
+        this.updateDate = updateDate;
+        return this;
+    }
+
+    public Boolean getSoftDelete() {
+        return softDelete;
+    }
+
+    public User setSoftDelete(Boolean softDelete) {
+        this.softDelete = softDelete;
+        return this;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
--- src/main/java/com/alkemy/wallet/security/SecurityConfiguration.java
+++ src/main/java/com/alkemy/wallet/security/SecurityConfiguration.java
@@ -6,12 +6,16 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfiguration {
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
 
     @Autowired
     private UserService userService;
@@ -22,6 +26,14 @@ public class SecurityConfiguration {
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(userService);
+    }
+
+    @Autowired
+    private JWTRequestFilter jwtRequestFilter;
+
+    @Override
+    protected void configure(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
     }
 
     @Bean

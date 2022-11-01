55b9c99f30cf498a81672ef5eb1d9fa53f019e43
stvteclas <stvpablo@gmail.com>
2022-11-01 17:08:36 -0300

    database configuration- create entity account and repo

---
+++ src/main/java/com/alkemy/wallet/model/entity/Account.java
@@ -0,0 +1,53 @@
+package com.alkemy.wallet.model.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.sun.istack.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.*;
+import org.springframework.format.annotation.NumberFormat;
+
+import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import java.time.LocalDateTime;
+
+
+@Getter
+@Setter
+@Entity
+@Table(name = "accounts")
+@SQLDelete(sql = "UPDATE accounts SET deleted = true WHERE id=?")
+@Where(clause = "softDelete=false")
+public class Account {
+    private static final long serialVersionUID = 1L;
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "account_id")
+    private Long accountId;
+    @Column(name = "currency")
+    @NumberFormat(style = NumberFormat.Style.CURRENCY)
+    @NotNull
+    private Double currency;
+    @Column(name = "transaction_limit")
+    @NotNull
+    private Double transactionLimit;
+    @Column(name = "balance")
+    @NotNull
+    private Double balance;
+    @Column(name = "timestamp")
+    @CreationTimestamp
+    private LocalDateTime createDateTime;
+    @UpdateTimestamp
+    private LocalDateTime updateDateTime;
+    //@ManyToOne(fetch = FetchType.LAZY, optional = false)
+    //@JoinColumn(name = "userId", nullable = false)
+    //@OnDelete(action = OnDeleteAction.CASCADE)
+    //@JsonIgnore
+    //private User user;
+    private boolean softDelete = Boolean.FALSE;
+
+
+}

272254c7f129d9b2dcb19a0a6ecad98563a7db8d
Juan Pablo Bernal <89616779+JuanToledoCol@users.noreply.github.com>
2022-11-01 12:53:42 -0500

    Created user model and your repository. (#46)

---
+++ src/main/java/com/alkemy/wallet/model/User.java
@@ -0,0 +1,40 @@
+package com.alkemy.wallet.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.Instant;
+
+@Setter
+@Getter
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_user", nullable = false)
+    private Integer id;
+
+    @Column(name = "first_name", nullable = false, length = 45)
+    private String firstName;
+
+    @Column(name = "last_name", nullable = false, length = 45)
+    private String lastName;
+
+    @Column(name = "email", nullable = false, length = 45)
+    private String email;
+
+    @Column(name = "password", nullable = false, length = 45)
+    private String password;
+
+    @Column(name = "creation_date", nullable = false)
+    private Instant creationDate;
+
+    @Column(name = "update_date")
+    private Instant updateDate;
+
+    @Column(name = "soft_delete")
+    private Byte softDelete;
+
+}
---
+++ src/main/java/com/alkemy/wallet/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package com.alkemy.wallet.repository;
+
+import com.alkemy.wallet.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+
+}

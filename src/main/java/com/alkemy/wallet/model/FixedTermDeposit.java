0b92bb9b7e5a9da1ca11e477f7fe4f575327b89a
EMG2311 <104037360+EMG2311@users.noreply.github.com>
2022-11-01 20:21:38 -0300

    Modifique Users

--- src/main/java/com/alkemy/wallet/model/FixedTermDeposit.java
+++ src/main/java/com/alkemy/wallet/model/FixedTermDeposit.java
@@ -1,5 +1,8 @@
 package com.alkemy.wallet.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.*;
 import java.sql.Timestamp;
 
@@ -10,9 +13,11 @@ public class FixedTermDeposit {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "FIXED_TERM_DEPOSIT_ID")
+    @Getter @Setter
     private Integer fixedTermDepositId;
 
     @Column(name = "AMOUNT", nullable = false)
+    @Getter@Setter
     private Double amount;
 
     @ManyToOne( fetch = FetchType.LAZY, cascade = CascadeType.PERSIST )
@@ -20,9 +25,12 @@ public class FixedTermDeposit {
     private Account account;
 
     @Column(name = "INTEREST", nullable = false)
+    @Getter@Setter
     private Double interest;
 
     // TODO: check timestamps format and auditing
+    @Getter@Setter
     private Timestamp creationDate;
+    @Getter@Setter
     private Timestamp closingDate;
 }

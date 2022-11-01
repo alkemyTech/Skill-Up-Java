6bd61b109ca67d4c037d581d4ffe66476fbdf1f2
Juan Pablo Bernal Toledo <jpbernal34@misena.edu.co>
2022-11-01 13:39:17 -0500

    SB-5: Created FixedTermDeposit model and your repository.

---
+++ src/main/java/com/alkemy/wallet/model/FixedTermDeposit.java
@@ -0,0 +1,38 @@
+package com.alkemy.wallet.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.Instant;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "fixed_term_deposit")
+public class FixedTermDeposit {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_fixed_term_deposit", nullable = false)
+    private Integer id;
+
+    @Column(name = "amount", nullable = false)
+    private Double amount;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "account_id", nullable = false)
+    private Account account;
+
+    @Column(name = "interest", nullable = false)
+    private Double interest;
+
+    @Column(name = "creation_date", nullable = false)
+    private Instant creationDate;
+
+    @Column(name = "update_date")
+    private Instant updateDate;
+}

8f41c963e70aee8a9e9a1e893ea2f79cbf512370
Matias Vicente <107733627+Matidx@users.noreply.github.com>
2022-11-01 15:06:01 -0300

    Created Role model and your repository (#47)

---
+++ src/main/java/com/alkemy/wallet/model/Role.java
@@ -0,0 +1,30 @@
+package com.alkemy.wallet.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.Instant;
+@Getter
+@Setter
+@Entity
+@Table(name = "roles")
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_rol", nullable = false)
+    private Integer id;
+
+    @Lob
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "description", length = 80)
+    private String description;
+
+    @Column(name = "creation_date", nullable = false)
+    private Instant creationDate;
+
+    @Column(name = "update_date")
+    private Instant updateDate;
+}

7db4a661877f8a9964096970bed8ab4cdb34ec74
EMG2311 <104037360+EMG2311@users.noreply.github.com>
2022-11-02 09:17:08 -0300

    changes in FixedTermDepositDto

--- src/main/java/com/alkemy/wallet/dto/FixedTermDepositDto.java
+++ src/main/java/com/alkemy/wallet/dto/FixedTermDepositDto.java
@@ -1,18 +1,23 @@
 package com.alkemy.wallet.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.Setter;
 
 import javax.validation.constraints.NotBlank;
+import java.sql.Date;
 import java.sql.Timestamp;
 
 public class FixedTermDepositDto {
     @Getter@Setter
+    @JsonProperty("fixedTermDepositId")
     private Integer fixedTermDepositId;
     @Getter@Setter
+    @JsonProperty("amount")
     private Double amount;
     @Getter@Setter
-    private Double interest;
-    @Getter@Setter
+    @JsonProperty("closingDate")
+    @JsonFormat(pattern="yyyy-MM-dd")
     private Timestamp closingDate;
 }

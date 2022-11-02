eb2ced42f89ce8011159b65b2bad8303b81927b6
Manuel Pautasso <manuelpautasso@gmail.com>
2022-11-01 21:01:34 -0300

    Fix custom query

--- src/main/java/com/alkemy/wallet/repository/IAccountRepository.java
+++ src/main/java/com/alkemy/wallet/repository/IAccountRepository.java
@@ -2,13 +2,15 @@ package com.alkemy.wallet.repository;
 
 import com.alkemy.wallet.model.Account;
 import com.alkemy.wallet.model.Currency;
+import com.alkemy.wallet.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 
 @Repository
 public interface IAccountRepository extends JpaRepository<Account, Integer> {
-    @Query("SELECT * FROM Account a WHERE a.userId = ?1 AND a.currency = ?2")
-    boolean existAccountByUserIdAndCurrency(int userId, Currency currency);
+   @Query("SELECT a.accountId FROM Account a WHERE a.user = ?1 AND a.currency = ?2")
+   Optional<Account> findAccountByUserIdAndCurrency(User user, Currency currency);
 }

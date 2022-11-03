338857b8550b5501c0d3cd159b0f8043d889c935
EMG2311 <104037360+EMG2311@users.noreply.github.com>
2022-11-03 16:56:21 -0300

    create getters and setters

--- src/main/java/com/alkemy/wallet/dto/FixedTermDepositDto.java
+++ src/main/java/com/alkemy/wallet/dto/FixedTermDepositDto.java
@@ -5,18 +5,14 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.validation.constraints.NotBlank;
-import java.sql.Date;
 import java.sql.Timestamp;
-
+@Getter@Setter
 public class FixedTermDepositDto {
-    @Getter@Setter
     @JsonProperty("fixedTermDepositId")
     private Integer fixedTermDepositId;
-    @Getter@Setter
     @JsonProperty("amount")
     private Double amount;
-    @Getter@Setter
+    private Integer account;
     @JsonProperty("closingDate")
     @JsonFormat(pattern="yyyy-MM-dd")
     private Timestamp closingDate;

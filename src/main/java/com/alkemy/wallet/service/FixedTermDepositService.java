f4d671ab4135375306b90145342917bf26a9c0ab
EMG2311 <104037360+EMG2311@users.noreply.github.com>
2022-11-03 16:59:39 -0300

    change return and add exception

--- src/main/java/com/alkemy/wallet/service/FixedTermDepositService.java
+++ src/main/java/com/alkemy/wallet/service/FixedTermDepositService.java
@@ -1,9 +1,10 @@
 package com.alkemy.wallet.service;
 
 import com.alkemy.wallet.dto.FixedTermDepositDto;
+import com.alkemy.wallet.exception.FixedTermDepositException;
 import com.alkemy.wallet.model.FixedTermDeposit;
 
 public interface FixedTermDepositService {
-    Integer createFixedTermDeposit(FixedTermDepositDto fixedTermDepositDto);
+    FixedTermDeposit createFixedTermDeposit(FixedTermDepositDto fixedTermDepositDto) throws FixedTermDepositException;
 
 }

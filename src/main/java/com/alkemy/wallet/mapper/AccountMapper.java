c1e27557b4e275569042bd5398f03f0a96a66f50
EMG2311 <104037360+EMG2311@users.noreply.github.com>
2022-11-04 15:08:16 -0300

    none

--- src/main/java/com/alkemy/wallet/mapper/AccountMapper.java
+++ src/main/java/com/alkemy/wallet/mapper/AccountMapper.java
@@ -1,13 +1,19 @@
 package com.alkemy.wallet.mapper;
 
 import com.alkemy.wallet.dto.AccountDto;
+import com.alkemy.wallet.dto.FixedTermDepositDto;
 import com.alkemy.wallet.model.Account;
+import com.alkemy.wallet.model.FixedTermDeposit;
 import lombok.RequiredArgsConstructor;
+import org.apache.catalina.mapper.Mapper;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
 public class AccountMapper {
+
+
     public AccountDto convertToDto(Account account) {
         return new AccountDto(
                 account.getAccountId(),
@@ -19,4 +25,7 @@ public class AccountMapper {
                 account.getUpdateDate() != null ? account.getUpdateDate().toString() : "null",
                 account.getSoftDelete());
     }
+
+
+
 }

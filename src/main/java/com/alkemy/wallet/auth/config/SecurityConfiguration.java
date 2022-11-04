cda38515329fdc02148a2372cd5913d8b24c4232
agustin-kiryk <agustin.kiryk@gmail.com>
2022-11-04 15:00:21 -0300

    SP1-00# : project structure

---
+++ src/main/java/com/alkemy/wallet/auth/config/SecurityConfiguration.java
@@ -0,0 +1,8 @@
+package com.alkemy.wallet.auth.config;
+
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@EnableWebSecurity
+public class SecurityConfiguration {
+
+}
---
+++ src/main/java/com/alkemy/wallet/controller/repository/UserRepository.java
@@ -0,0 +1,5 @@
+package com.alkemy.wallet.controller.repository;
+
+public interface UserRepository {
+
+}

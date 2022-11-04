634f58d21b13821c9f5600c7e429ea0aedd381e4
EMG2311 <104037360+EMG2311@users.noreply.github.com>
2022-11-04 15:08:46 -0300

    add findAccountByUserIdAndCurrency

--- src/main/java/com/alkemy/wallet/service/AccountService.java
+++ src/main/java/com/alkemy/wallet/service/AccountService.java
@@ -1,10 +1,13 @@
 package com.alkemy.wallet.service;
 
 import com.alkemy.wallet.dto.AccountDto;
+import com.alkemy.wallet.model.Account;
 import com.alkemy.wallet.model.Currency;
+import com.alkemy.wallet.model.User;
 
 
 public interface AccountService {
     AccountDto createAccount(int userId, Currency currency);
     AccountDto reduceBalance(int accountId, double amount);
+    Account findAccountByUserIdAndCurrency(User user, Currency currency);
 }

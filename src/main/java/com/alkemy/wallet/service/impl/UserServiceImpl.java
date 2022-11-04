a87cf0524017feaed16ae945a25f7df8a77af4af
JulioCM <julyomanzano@gmail.com>
2022-11-03 23:08:31 -0300

    Resolve Conflict

--- src/main/java/com/alkemy/wallet/service/impl/UserServiceImpl.java
+++ src/main/java/com/alkemy/wallet/service/impl/UserServiceImpl.java
@@ -18,7 +18,7 @@ import java.util.List;
 
 
 @Service
-public class UserServiceImpl implements UserService {
+public class UserServiceImpl implements IUserService {
 
     @Autowired
     UserRepository userRepository;

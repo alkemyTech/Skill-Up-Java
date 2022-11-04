85c5ea9869e1e4b3cc785f2371a63e5a1862f228
EMG2311 <104037360+EMG2311@users.noreply.github.com>
2022-11-04 15:08:05 -0300

    add getters and setters

--- src/main/java/com/alkemy/wallet/dto/AccountDto.java
+++ src/main/java/com/alkemy/wallet/dto/AccountDto.java
@@ -2,11 +2,15 @@ package com.alkemy.wallet.dto;
 
 import com.alkemy.wallet.model.Currency;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+import lombok.Setter;
+
 
 public record AccountDto(
         @JsonProperty("id") Integer id,
         @JsonProperty("userId") Integer userId,
         @JsonProperty("balance") Double balance,
+        @Getter@Setter
         @JsonProperty("currency") Currency currency,
         @JsonProperty("transactionLimit") Double transactionLimit,
         @JsonProperty("creationDate") String creationDate,

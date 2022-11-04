cd17c22ab5ef2dd9d212ad0650988ebb6e7161da
EMG2311 <104037360+EMG2311@users.noreply.github.com>
2022-11-03 22:43:06 -0300

    correction

--- src/main/java/com/alkemy/wallet/controller/FixedTermDepositController.java
+++ src/main/java/com/alkemy/wallet/controller/FixedTermDepositController.java
@@ -5,6 +5,8 @@ import com.alkemy.wallet.exception.FixedTermDepositException;
 import com.alkemy.wallet.service.FixedTermDepositService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -23,9 +25,10 @@ public class FixedTermDepositController {
 
     @PostMapping("/create")
     @PreAuthorize("hasRole('USER_ROLE')")
-    public void createFixedTermDeposit(@Validated @RequestBody FixedTermDepositDto fixedTermDepositDto) throws FixedTermDepositException {
+    public ResponseEntity<String> createFixedTermDeposit(@Validated @RequestBody FixedTermDepositDto fixedTermDepositDto) throws FixedTermDepositException {
         fixedTermDepositDto.setClosingDate(new Timestamp(fixedTermDepositDto.getClosingDate().getTime()+86400000));
         fixedTermDepositService.createFixedTermDeposit(fixedTermDepositDto);
+        return ResponseEntity.status(HttpStatus.CREATED).body("Successful creation of an FixedTermDeposit.");
 
     }
 

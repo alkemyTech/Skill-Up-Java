892b1cca2f5f306491cfad6c5e4fddfdeaebcbcc
Jeremias <JeremiasOviedo.Dev@gmail.com>
2022-11-04 16:59:01 -0300

    SP1-2: Created the User Model, Repo, Dto and Mapper. (#8)
    * SP1-2: Created the User entity
    
    * SP1-2: Created the User repository with custom method
    
    * SP1-2: Created the UserDto class.
    
    * SP1-2: Created the UserMap class.
    
    * SP1-2: Created the UserService interface and class.
    
    * Fixed minor typos.
    
    * SP1-2: fixed typos.

--- src/main/java/com/alkemy/wallet/repository/UserRepository.java
+++ src/main/java/com/alkemy/wallet/repository/UserRepository.java
@@ -1,5 +1,19 @@
 package com.alkemy.wallet.repository;
 
-public interface UserRepository {
+
+import com.alkemy.wallet.entity.UserEntity;
+import java.util.List;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<UserEntity, Long> {
+
+  List<UserEntity> findAll(Specification<UserEntity> spec);
+
+  UserEntity findByUsername(String email);
+
 
 }
+

837ab5a0eeae99625cc5e259d170c28182307f9f
ale94 <alejandrorua111@gmail.com>
2022-11-04 17:11:25 -0300

    SP1-5: add TransactionRepository

--- src/main/java/com/alkemy/wallet/repository/TransactionRepository.java
+++ src/main/java/com/alkemy/wallet/repository/TransactionRepository.java
@@ -1,6 +1,10 @@
 package com.alkemy.wallet.repository;
 
+import com.alkemy.wallet.entity.TransactionsEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface TransactionRepository {
+@Repository
+public interface TransactionRepository extends JpaRepository<Long, TransactionsEntity> {
 
 }

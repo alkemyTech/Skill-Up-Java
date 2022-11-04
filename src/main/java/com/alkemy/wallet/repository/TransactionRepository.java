e77217b0bad333edee0190a0a31a6fb4678c8f09
Brian Polesel <brianpolesel07@gmail.com>
2022-11-04 17:30:04 -0300

    add dependency and method in TransactionRepository

--- src/main/java/com/alkemy/wallet/repository/TransactionRepository.java
+++ src/main/java/com/alkemy/wallet/repository/TransactionRepository.java
@@ -1,9 +1,24 @@
 package com.alkemy.wallet.repository;
 
 import com.alkemy.wallet.model.Transaction;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Repository
 public interface TransactionRepository extends JpaRepository<Transaction, Integer> {
+    //@Query("select transaction where account_id = :userId ")
+    //List<Transaction> findAllByUser(@Param(value="userId") Integer id, Pageable pageable);
+
+    @Modifying
+    @Transactional
+    @Query("update Transaction u set u.description = :description where u.id = :id")
+    void updateDescription(@Param(value = "id") Integer id, @Param(value = "description") String description);
+
 }

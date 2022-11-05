5a66d163c0fc54ff00b1234f0160f8de8574f766
Manuel Pautasso <manuelpautasso@gmail.com>
2022-11-05 12:44:18 -0300

    Added method to create accounts with username and find account by userId or userId and currency

--- src/main/java/com/alkemy/wallet/service/AccountService.java
+++ src/main/java/com/alkemy/wallet/service/AccountService.java
@@ -1,14 +1,24 @@
 package com.alkemy.wallet.service;
 
 import com.alkemy.wallet.dto.AccountDto;
+import com.alkemy.wallet.dto.CurrencyRequestDto;
 import com.alkemy.wallet.model.Account;
 import com.alkemy.wallet.model.Currency;
 
+import java.util.List;
 
 public interface AccountService {
-    AccountDto createAccount(int userId, Currency currency);
+    AccountDto createAccountByUserId(int userId, Currency currency);
+    AccountDto createAccountByUsername(String username, Currency currency);
+
     AccountDto reduceBalance(int accountId, double amount);
 
     AccountDto increaseBalance(int accountId, double amount);
     AccountDto getAccountById(Integer accountId);
+
+    List<AccountDto> getAccountsByUserId(int userId);
+
+    AccountDto getAccountByUserAndCurrency(int userId, CurrencyRequestDto currencyRequestDto);
+
+
 }

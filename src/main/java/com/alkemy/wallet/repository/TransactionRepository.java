a0c213d25460425f779264b8e6faca75e6bd81e8
Manuel Pautasso <manuelpautasso@gmail.com>
2022-11-05 12:05:21 -0300

    Added method to find transactions for an account id on the repository

--- src/main/java/com/alkemy/wallet/repository/TransactionRepository.java
+++ src/main/java/com/alkemy/wallet/repository/TransactionRepository.java
@@ -1,10 +1,15 @@
 package com.alkemy.wallet.repository;
 
+import com.alkemy.wallet.model.Account;
 import com.alkemy.wallet.model.Transaction;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface TransactionRepository extends JpaRepository<Transaction, Integer> {
-
+    @Query("SELECT t FROM Transaction t WHERE t.account = ?1")
+    List<Transaction> findallByAccountId(Account account);
 }

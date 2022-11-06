9e5c2b8e6290c6efd4b3ff2148ed974f985d9333
Manuel Pautasso <manuelpautasso@gmail.com>
2022-11-06 20:10:55 -0300

    Changed the response entity to return AccountDto on create accounts

--- src/main/java/com/alkemy/wallet/controller/AccountController.java
+++ src/main/java/com/alkemy/wallet/controller/AccountController.java
@@ -1,15 +1,18 @@
 package com.alkemy.wallet.controller;
 
 import com.alkemy.wallet.dto.AccountBalanceDto;
+import com.alkemy.wallet.dto.AccountDto;
 import com.alkemy.wallet.dto.CurrencyRequestDto;
 import com.alkemy.wallet.service.AccountService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Slf4j
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/accounts")
@@ -17,9 +20,9 @@ public class AccountController {
     private final AccountService accountService;
 
     @PostMapping
-    public ResponseEntity<String> createAccount(@RequestBody CurrencyRequestDto currency, @RequestHeader("Authorization") String token) {
-        accountService.createAccount(token, currency);
-        return ResponseEntity.status(HttpStatus.CREATED).body("Successful creation of an account.");
+    public ResponseEntity<AccountDto> createAccount(@RequestBody CurrencyRequestDto currency, @RequestHeader("Authorization") String token) {
+        AccountDto account = accountService.createAccount(token, currency);
+        return ResponseEntity.status(HttpStatus.CREATED).body(account);
     }
 
     @GetMapping("/balance")

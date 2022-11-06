2169ed672c6ef572486bc7d3415566605623abba
agustin-kiryk <agustin.kiryk@gmail.com>
2022-11-06 18:57:47 -0300

    Resolve anotations map

--- src/main/java/com/alkemy/wallet/mapper/RoleMap.java
+++ src/main/java/com/alkemy/wallet/mapper/RoleMap.java
@@ -2,7 +2,9 @@ package com.alkemy.wallet.mapper;
 
 import com.alkemy.wallet.dto.RoleDto;
 import com.alkemy.wallet.entity.RoleEntity;
+import org.springframework.stereotype.Component;
 
+@Component
 public class RoleMap {
 
   public RoleEntity roleDto2Entity(RoleDto role) {

00d059bcbf9d3d799eff5e0d80bc32885ef7bb26
Manuel Pautasso <manuelpautasso@gmail.com>
2022-11-06 14:04:00 -0300

    Added endpoint to get balance and updated createAccount to get the token from de request

--- src/main/java/com/alkemy/wallet/controller/AccountController.java
+++ src/main/java/com/alkemy/wallet/controller/AccountController.java
@@ -1,11 +1,14 @@
 package com.alkemy.wallet.controller;
 
+import com.alkemy.wallet.dto.AccountBalanceDto;
 import com.alkemy.wallet.dto.CurrencyRequestDto;
 import com.alkemy.wallet.service.AccountService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
@@ -14,10 +17,13 @@ public class AccountController {
     private final AccountService accountService;
 
     @PostMapping
-    public ResponseEntity<String> createAccount(@RequestBody CurrencyRequestDto currency){
-        //Have to get the userId from the authentication context
-        int userId = 1;
-        accountService.createAccount(userId, currency.currencyRequestToEnum());
+    public ResponseEntity<String> createAccount(@RequestBody CurrencyRequestDto currency, @RequestHeader("Authorization") String token) {
+        accountService.createAccount(token, currency);
         return ResponseEntity.status(HttpStatus.CREATED).body("Successful creation of an account.");
+    }
+
+    @GetMapping("/balance")
+    public ResponseEntity<List<AccountBalanceDto>> getBalance(@RequestHeader("Authorization") String token) {
+        return ResponseEntity.ok(accountService.getUserBalance(token));
     }
 }

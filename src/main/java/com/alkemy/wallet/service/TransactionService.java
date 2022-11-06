95e5d7055ae219cd6f429238228cc68eae4633ca
Manuel Pautasso <manuelpautasso@gmail.com>
2022-11-06 13:53:21 -0300

    Added method to get transactions by Account

--- src/main/java/com/alkemy/wallet/service/TransactionService.java
+++ src/main/java/com/alkemy/wallet/service/TransactionService.java
@@ -11,7 +11,6 @@ import java.util.List;
 
 public interface TransactionService {
     TransactionDetailDto getTransactionDetailById (Integer Id) throws Exception;
-    List<TransactionDetailDto> getTransactionsDetailByAccountId (Integer accountId);
 
     TransactionDepositDto createDeposit(TransactionDepositRequestDto transactionDepositRequestDto);
 

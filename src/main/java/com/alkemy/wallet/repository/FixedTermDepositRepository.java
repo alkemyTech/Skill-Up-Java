f4d3a4dbb15801883ce5ec4a15a20b6f8c1e22e1
Manuel Pautasso <manuelpautasso@gmail.com>
2022-11-06 13:55:42 -0300

    Added custom query to get all FixedTermDeposit by Account on the FixedTermDepositRepository

--- src/main/java/com/alkemy/wallet/repository/FixedTermDepositRepository.java
+++ src/main/java/com/alkemy/wallet/repository/FixedTermDepositRepository.java
@@ -1,11 +1,16 @@
 package com.alkemy.wallet.repository;
 
+import com.alkemy.wallet.model.Account;
 import com.alkemy.wallet.model.FixedTermDeposit;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
-@Repository
-public interface FixedTermDepositRepository extends JpaRepository<FixedTermDeposit,Integer> {
+import java.util.List;
 
+@Repository
+public interface FixedTermDepositRepository extends JpaRepository<FixedTermDeposit, Integer> {
+    @Query("SELECT f FROM FixedTermDeposit f WHERE f.account = ?1")
+    List<FixedTermDeposit> findallByAccount(Account account);
 
 }

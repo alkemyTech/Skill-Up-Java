ab4f711bdab949cb4f64f05360fd6912e709cb00
Micael <micaelcapo1316@gmail.com>
2022-11-06 19:18:43 -0300

    feature/Endpoint_get_detail_of_a_transaction: Modified UserService, added new method getUserByTransactionId

--- src/main/java/com/alkemy/wallet/service/TransactionService.java
+++ src/main/java/com/alkemy/wallet/service/TransactionService.java
@@ -3,10 +3,8 @@ package com.alkemy.wallet.service;
 import com.alkemy.wallet.dto.TransactionDepositDto;
 import com.alkemy.wallet.dto.TransactionDepositRequestDto;
 import com.alkemy.wallet.dto.TransactionDetailDto;
-
 import com.alkemy.wallet.dto.TransactionPatchDto;
-import com.alkemy.wallet.model.Transaction;
-
+import com.alkemy.wallet.model.User;
 import java.util.List;
 
 public interface TransactionService {
@@ -17,5 +15,8 @@ public interface TransactionService {
     TransactionDetailDto updateTransaction(TransactionPatchDto transaction, Integer Id) throws Exception;
 
     List<TransactionDetailDto> getTransactions(Integer userId);
+
+    User getUserByTransactionId(Integer id);
+
 
 }

3f34679ce25dcd965daa49836dcd1876522cf03f
will <wilbertojrr@gmail.com>
2022-11-08 14:00:09 -0300

    SP1-20:fix userID

--- src/main/java/com/alkemy/wallet/service/impl/FixedTermServiceImpl.java
+++ src/main/java/com/alkemy/wallet/service/impl/FixedTermServiceImpl.java
@@ -2,12 +2,15 @@ package com.alkemy.wallet.service.impl;
 
 import com.alkemy.wallet.dto.FixedTermDepositDto;
 import com.alkemy.wallet.entity.FixedTermDepositEntity;
+import com.alkemy.wallet.entity.UserEntity;
 import com.alkemy.wallet.mapper.exception.MinDaysException;
 import com.alkemy.wallet.mapper.FixedTermDepositMap;
 import com.alkemy.wallet.repository.IFixedTermDepositRepository;
+import com.alkemy.wallet.repository.IUserRepository;
 import com.alkemy.wallet.service.IAccountService;
 import com.alkemy.wallet.service.IFixedTermDepositService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 import java.util.Date;
@@ -23,10 +26,20 @@ public class FixedTermServiceImpl implements IFixedTermDepositService {
     @Autowired
     private IAccountService accountService;
 
+    @Autowired
+    private IUserRepository userRepository;
+
     @Override
     public FixedTermDepositDto createNewFixedTerm(FixedTermDepositDto dto) {
 
         FixedTermDepositEntity entity = fixedTermDepositMap.fixedTermDepositDto2Entity(dto);
+
+        String email = SecurityContextHolder.getContext().getAuthentication().getName();
+        UserEntity user = userRepository.findByEmail(email);
+        entity.setUserId(user.getUserId());
+
+        entity.setAccountId(20L);
+
         entity.setCreationDate(new Date());
 
         Long minDays = ((entity.getCreationDate().getTime() - entity.getClosingDate().getTime())
@@ -35,9 +48,11 @@ public class FixedTermServiceImpl implements IFixedTermDepositService {
             throw new MinDaysException("The Closing date cannot be less than 30 days.");
         }
 
-        entity.setInterest(minDays * 0.5);
+        entity.setInterest((minDays * 0.5)*-1);
 
-        accountService.updateBalance(entity.getAccountId(), entity.getAmount());
+        Long id = 20L;
+
+        accountService.updateBalance(id, entity.getAmount());
         FixedTermDepositEntity CreatedFixedDeposit = fixedTermDepositRepository.save(entity);
         return fixedTermDepositMap.fixedTermDepositEntity2Dto(CreatedFixedDeposit);
     }

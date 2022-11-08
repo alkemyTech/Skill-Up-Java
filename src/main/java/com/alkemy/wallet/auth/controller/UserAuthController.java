6ddea4aa88fe40ec980d8f4ed8ace51de87b5835
Agustin Kiryk <111461382+agustin-kiryk@users.noreply.github.com>
2022-11-08 14:03:09 -0300

    SP1-10# :  Endpoint user delete (#35)
    * SP1-22 # : Add method getDetailsById in transacion controller
    
    * SP1-10 # : Add delete account
    
    * Fix: add security in delete
    
    * fix: add git ignore aplication propierties
    
    * fix: resolve error soft delete change overryde method deleteById in UserService

--- src/main/java/com/alkemy/wallet/auth/controller/UserAuthController.java
+++ src/main/java/com/alkemy/wallet/auth/controller/UserAuthController.java
@@ -14,6 +14,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -56,5 +57,7 @@ public class UserAuthController {
     final String jwt = jwtTokenUtils.generateToken(userDetails);
     return ResponseEntity.ok(new AuthenticationResponse(jwt));
   }
+
+
 
 }
--- src/main/java/com/alkemy/wallet/controller/UserController.java
+++ src/main/java/com/alkemy/wallet/controller/UserController.java
@@ -4,8 +4,11 @@ import com.alkemy.wallet.dto.UserDto;
 import com.alkemy.wallet.service.IUserService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -20,6 +23,12 @@ public class UserController {
   {
     List<UserDto> users= userService.listAllUsers();
     return ResponseEntity.ok().body(users);
+  }
+
+  @DeleteMapping("/users/{id}")
+  public ResponseEntity<Void> deleteUser(@PathVariable("id") Long id){
+    this.userService.delete(id);
+    return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
   }
 
 

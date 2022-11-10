c0f8e9d13d67fbb5e46a8320a6e30af14e2d68df
stvteclas <stvpablo@gmail.com>
2022-11-10 10:15:54 -0300

    fix code

--- src/main/java/com/alkemy/wallet/controller/TransactionController.java
+++ src/main/java/com/alkemy/wallet/controller/TransactionController.java
@@ -4,13 +4,11 @@ import com.alkemy.wallet.dto.TransactionDTO;
 import com.alkemy.wallet.service.ITransactionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-//@Secured({"ROLE_ADMIN","ROLE_USER"})
-@PreAuthorize("hasRole('USER') OR hasRole('ADMIN')")
+
 @RestController
 @RequestMapping("/transactions")
 public class TransactionController {
@@ -19,7 +17,7 @@ public class TransactionController {
     private ITransactionService transactionService;
 
 
-    @PreAuthorize("hasRole('ADMIN')")
+
     @PostMapping("/deposit")
     public ResponseEntity<Object> deposit(@RequestBody TransactionDTO transactionDTO) {
         return transactionService.saveDeposit(transactionDTO);

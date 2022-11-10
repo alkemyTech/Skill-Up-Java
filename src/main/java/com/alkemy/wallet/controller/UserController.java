84125e06d6ecb803a4dede87d800d07932a36f85
Juan Pablo Bernal <89616779+JuanToledoCol@users.noreply.github.com>
2022-11-10 18:42:51 -0500

    SB-30: Agregar documentacion con swagger (#100)
    * SB-30: Create Documentation with Swagger. Create the Configuration
    
    * SB-30: Endopoint User list documented.
    
    * SB-30: Endopoint User list documented.

--- src/main/java/com/alkemy/wallet/controller/UserController.java
+++ src/main/java/com/alkemy/wallet/controller/UserController.java
@@ -3,6 +3,14 @@ package com.alkemy.wallet.controller;
 import com.alkemy.wallet.dto.UserDTO;
 import com.alkemy.wallet.model.User;
 import com.alkemy.wallet.service.IUserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.Parameters;
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -16,7 +24,16 @@ public class UserController {
     @Autowired
     private IUserService userService;
 
-    @GetMapping
+    //Documentation--------------------------------
+    @Operation(summary = "User list", description = "<h3>Endpoint that lists the users that are registered in the database.</h3>" +
+            "<p>You can use the pagination that lists maximum 10 users for page." +
+            "</br><b>Note: </b>if You don't pass it the pagination parameter, it will list all the existing users in the database.</p>")
+    @Parameters(value = { @Parameter(name = "page", description = "Number of page", example = "1", in = ParameterIn.QUERY)})
+    @ApiResponses(value = { @ApiResponse(responseCode = "200", description = "Found Users", content = {
+            @Content(mediaType = "application/json", schema = @Schema(implementation = UserDTO.class))}),
+            @ApiResponse(responseCode="401", description = "Unauthorizated")})
+    //Mapping---------------------------------------
+    @GetMapping()
     public ResponseEntity<List<UserDTO>> getUsers(@RequestParam(value = "page"  , required = false) Integer page){
         List<UserDTO> users;
         users =  page !=null ? userService.getUsersByPage(page) : userService.getAllUsers();

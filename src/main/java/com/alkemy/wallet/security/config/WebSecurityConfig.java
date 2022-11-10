2fd22f798d0ea8f04c3b3bcb23c7e998411c4b09
Cokke <coke.jgomez@gmail.com>
2022-11-09 22:41:57 -0300

    Feature/Implement JWT SB-7 (#97)
    * Add JWT dependencies
    
    * al crear un usuario retorna un token
    
    * Se agrega la logica que devuelve el token al registrar un user
    
    * Se modifica aplication properties
    
    * Se modifica aplication properties para swagger
    
    * Se modifica la aplication.properties
    
    Co-authored-by: Matias Vicente <107733627+Matidx@users.noreply.github.com>

---
+++ src/main/java/com/alkemy/wallet/security/config/WebSecurityConfig.java
@@ -0,0 +1,52 @@
+package com.alkemy.wallet.security.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+import com.alkemy.wallet.security.config.JwtTokenFilterConfigurer;
+import com.alkemy.wallet.security.config.JwtTokenProvider;
+
+@Configuration
+public class WebSecurityConfig {
+
+	@Autowired
+	private JwtTokenProvider jwtTokenProvider;
+	
+	//* aca se filtran los path de acuerdo a los accesos que tenga segun el rol asignado
+	 @Bean
+	 SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+		http.csrf().disable();
+		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+		http.authorizeRequests()
+		.antMatchers("/**").permitAll()
+		.antMatchers("/auth/login").permitAll()
+		.antMatchers("/auth/signup").permitAll()
+		.antMatchers("/auth/**").permitAll()
+		.antMatchers("/auth/register").permitAll()
+		.anyRequest().authenticated();
+		
+		http.exceptionHandling().accessDeniedPage("/login");
+		http.apply(new JwtTokenFilterConfigurer(jwtTokenProvider));
+		
+		return http.build();
+	}
+	
+	@Bean
+    AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+	
+	@Bean
+	PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder(12);
+	}
+	
+}
--- src/main/java/com/alkemy/wallet/service/IUserService.java
+++ src/main/java/com/alkemy/wallet/service/IUserService.java
@@ -3,10 +3,11 @@ package com.alkemy.wallet.service;
 
 import com.alkemy.wallet.dto.UserDTO;
 import com.alkemy.wallet.dto.UserRegisterDTO;
-import com.alkemy.wallet.model.User;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface IUserService {
     public List<UserDTO> getAllUsers();
@@ -14,6 +15,13 @@ public interface IUserService {
     public UserRegisterDTO createUser(UserDTO userDTO);
 
     boolean deleteUserById(Integer id);
+    
+  //Declaracion de metodos para la seguridad
+    public String login(String email, String password);
+    
+	public String signUp(User User);
+	
+	public UserDetails loadUserByUsername(String email);
 
     public List<UserDTO> getUsersByPage(Integer page);
 

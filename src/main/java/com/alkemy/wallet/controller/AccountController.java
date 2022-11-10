dc2b6da88d42207bf1b78332f4a4267c5a2b35eb
Micael Labria <91095444+MicaLabEGG@users.noreply.github.com>
2022-11-10 13:56:49 -0300

    feature/Added documentation to Accounts: Modified AccountController, nos has Swagger Documentation integrated (#43)

--- src/main/java/com/alkemy/wallet/controller/AccountController.java
+++ src/main/java/com/alkemy/wallet/controller/AccountController.java
@@ -2,6 +2,12 @@ package com.alkemy.wallet.controller;
 
 import com.alkemy.wallet.dto.*;
 import com.alkemy.wallet.service.AccountService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
@@ -17,19 +23,46 @@ import java.util.List;
 public class AccountController {
     private final AccountService accountService;
 
+    //Swagger Notation createAccount
+    @Operation(summary = "Create Account of a User")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Account created",
+            content = { @Content(mediaType = "application/json",
+            schema = @Schema(implementation = AccountDto.class)) })
+    })
+    //End Swagger notation
+
     @PostMapping
-    public ResponseEntity<AccountDto> createAccount(@RequestBody CurrencyRequestDto currency, @RequestHeader("Authorization") String token) {
+    public ResponseEntity<AccountDto> createAccount(@Parameter(description = "Currency that the Account has: USD or ARS ")@RequestBody CurrencyRequestDto currency, @Parameter(description = "authentication token") @RequestHeader("Authorization") String token) {
         AccountDto account = accountService.createAccount(token, currency);
         return ResponseEntity.status(HttpStatus.CREATED).body(account);
     }
+
+    //Swagger Notation getBalance
+    @Operation(summary = "Get balance of a Account")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Found Balances of Accounts",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = AccountBalanceDto.class)) })
+    })
+    //End Swagger notation
 
     @GetMapping("/balance")
     public ResponseEntity<List<AccountBalanceDto>> getBalance(@RequestHeader("Authorization") String token) {
         return ResponseEntity.ok(accountService.getUserBalance(token));
     }
 
+    //Swagger Notation getBalance
+    @Operation(summary = "Updates TransactionLimit of a Account")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Update successful",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = AccountDetailDto.class)) })
+    })
+    //End Swagger notation
+
     @PatchMapping(value="/{id}")
-    ResponseEntity<AccountDetailDto> updateAccount(@RequestBody AccountPatchDto accountPatchDto, @PathVariable Integer id, @RequestHeader("Authorization") String userToken) throws Exception {
+    ResponseEntity<AccountDetailDto> updateAccount(@RequestBody AccountPatchDto accountPatchDto, @Parameter(description = "id account to be updated") @PathVariable Integer id, @Parameter(description = "authentication token") @RequestHeader("Authorization") String userToken) throws Exception {
         return ResponseEntity.ok(accountService.updateAccount(accountPatchDto, id, userToken));
     }
 }

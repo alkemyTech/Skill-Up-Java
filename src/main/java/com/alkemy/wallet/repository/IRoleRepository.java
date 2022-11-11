e9afc98e1439fef757bd6e4465c57c92ad5472ed
Jeremias <JeremiasOviedo.Dev@gmail.com>
2022-11-11 00:52:46 -0300

    Fix/Roles: Fixed the user creation method. Now it assigns a role automatically (#49)

--- src/main/java/com/alkemy/wallet/repository/IRoleRepository.java
+++ src/main/java/com/alkemy/wallet/repository/IRoleRepository.java
@@ -1,13 +1,14 @@
 package com.alkemy.wallet.repository;
 
 import com.alkemy.wallet.entity.RoleEntity;
+import com.alkemy.wallet.enumeration.RoleName;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface IRoleRepository extends JpaRepository <RoleEntity, Long> {
 
-  RoleEntity findByName(String role);
+  RoleEntity findByName(RoleName name);
 }
 
 

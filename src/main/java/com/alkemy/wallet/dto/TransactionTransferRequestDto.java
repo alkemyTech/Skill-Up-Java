2f1fdb94958208d36866afafbd76e0a882406f9d
Manuel Pautasso <manuelpautasso@gmail.com>
2022-11-12 14:01:51 -0300

    Added constructors with all args and no args to Transaction, User, TransactionDetailDto and TransactionTransferRequestDto

--- src/main/java/com/alkemy/wallet/dto/TransactionTransferRequestDto.java
+++ src/main/java/com/alkemy/wallet/dto/TransactionTransferRequestDto.java
@@ -1,6 +1,7 @@
 package com.alkemy.wallet.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -9,6 +10,7 @@ import javax.validation.constraints.NotBlank;
 
 @Data
 @NoArgsConstructor
+@AllArgsConstructor
 public class TransactionTransferRequestDto {
 
     @JsonProperty( "amount")

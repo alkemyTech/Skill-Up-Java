3312a6da48003dcb89e8918d4e9db6e366eee013
Daniel <74437763+Danielonce@users.noreply.github.com>
2022-11-13 16:43:31 -0300

    openapi doc title and description (#117)

--- src/main/java/com/alkemy/wallet/WalletApplication.java
+++ src/main/java/com/alkemy/wallet/WalletApplication.java
@@ -1,12 +1,17 @@
 package com.alkemy.wallet;
 
 import com.alkemy.wallet.model.Account;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.util.logging.Logger;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "Spring Bugs", version = "1.0.0", description = "Wallet API"))
 public class WalletApplication {
 
 	public static void main(String[] args) {

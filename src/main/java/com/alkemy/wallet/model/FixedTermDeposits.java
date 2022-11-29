5b2ca32e92d4b5a47ef925f97ee2f0ef9a3079ce
AstroCaro <silvagarcesc@gmail.com>
2022-11-29 00:34:39 -0300

    Se agrega entidad FixedTermDeposit

---
+++ src/main/java/com/alkemy/wallet/model/FixedTermDeposits.java
@@ -0,0 +1,35 @@
+package com.alkemy.wallet.model;
+
+import com.sun.istack.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+
+
+@Getter
+@Setter
+@Entity
+public class FixedTermDeposits {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+
+    @NotNull
+    private Double amount;
+
+    @NotNull
+    private Double interest;
+
+    @NotNull
+    private Timestamp creationDate;
+
+    @NotNull
+    private Timestamp closingDate;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "account_id")
+    private Account account;
+}

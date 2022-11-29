8efae36f27a2496d6ba2837105276f15b9b5e663
AndresTabor <andrestaborda43@gmail.com>
2022-11-28 22:39:14 -0500

    añadiendo entidad Account

--- src/main/java/com/alkemy/wallet/model/Account.java
+++ src/main/java/com/alkemy/wallet/model/Account.java
@@ -1,6 +1,42 @@
 package com.alkemy.wallet.model;
 
+import lombok.Data;
+import lombok.NonNull;
 
+import javax.persistence.*;
+import java.sql.Timestamp;
+
+@Entity
+@Table(name = "account")
+@Data
 public class Account {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @NonNull
+    private String currency;
+
+    @NonNull
+    @Column(name="transaction_limit", nullable = false)
+    private Double transactionLimit;
+
+    @NonNull
+    private Double balance;
+
+    //TODO: establecer relación
+    private Long userId;
+
+    @NonNull
+    private Timestamp timestamp;
+
+    @NonNull
+    @Column(name="soft_delete", nullable = false)
+    private boolean softDelete;
+
+
+    public Account() {
+
+    }
 }

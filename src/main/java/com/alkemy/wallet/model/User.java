b97077728218cd82f9ccc20a6c9d1f6d4b61acec
Álvaro Aberastain Marzorati <a.aberastain.marzorati@gmail.com>
2022-11-29 00:25:23 -0300

    agregadas annotations y dependencia para validaciones

--- src/main/java/com/alkemy/wallet/model/User.java
+++ src/main/java/com/alkemy/wallet/model/User.java
@@ -2,6 +2,8 @@ package com.alkemy.wallet.model;
 
 import java.sql.Timestamp;
 import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
 
 @Entity
 @Table(name="users")
@@ -12,12 +14,17 @@ public class User {
     @GeneratedValue(strategy = GenerationType.AUTO)
 	private long userId;
 	@Column(name = "first_name", nullable=false)
+	@NotEmpty
 	private String firstName; 
 	@Column(name = "last_name", nullable=false)
+	@NotEmpty
 	private String lastName;
 	@Column(name = "email", unique=true, nullable=false)
+	@Email
+	@NotEmpty
 	private String email;
 	@Column(name = "password", nullable=false)
+	@NotEmpty
 	private String password;
 	@ManyToOne
 	private String roleId; // Clave foranea hacia ID de Role

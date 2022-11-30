ffeb4afbb0e09b1985e0d66ee2d72e06877f411a
Agustin Rios <agustin-rios26@hotmail.com>
2022-11-30 01:02:22 -0300

    Creando modelo Transactions

---
+++ src/main/java/com/alkemy/wallet/model/Transaction.java
@@ -0,0 +1,37 @@
+package com.alkemy.wallet.model;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.sql.Timestamp;
+
+@Entity
+@Data
+@Table(name="transaction")
+public class Transaction {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(name = "amount")
+    @NotNull
+    private Double amount;
+
+    @NotNull
+    @Enumerated(EnumType.STRING)
+    @Column(name = "type")
+    private TypeOfTransaction type;
+
+    @Column(name="description")
+    private String description;
+
+    @ManyToOne
+    @JoinColumn(name = "id_account")
+    Account account;
+
+    @Column(name = "timestamp")
+    private Timestamp timestamp;
+
+}
---
+++ src/main/java/com/alkemy/wallet/model/TypeOfTransaction.java
@@ -0,0 +1,7 @@
+package com.alkemy.wallet.model;
+
+public enum TypeOfTransaction {
+    income,
+    payment,
+    deposit
+}

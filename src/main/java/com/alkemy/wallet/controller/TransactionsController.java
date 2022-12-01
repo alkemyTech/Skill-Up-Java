23148a3244da7cf23ef4deb32d4f98baafc36260
Sissel <victorio.sarnaglia@gmail.com>
2022-12-01 18:07:22 -0300

    corrijo listado de transacciones

--- src/main/java/com/alkemy/wallet/controller/TransactionsController.java
+++ src/main/java/com/alkemy/wallet/controller/TransactionsController.java
@@ -2,6 +2,7 @@ package com.alkemy.wallet.controller;
 
 import com.alkemy.wallet.dto.TransactionDto;
 import com.alkemy.wallet.service.TransactionService;
+import com.alkemy.wallet.service.interfaces.IAccountService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,8 +15,11 @@ public class TransactionsController {
 
     private TransactionService transactionService;
 
+    private IAccountService accountService;
+
     @GetMapping("/transactions/{userId}")
     public HashSet<TransactionDto> getTransactions(@PathVariable("userId") Long id) {
-        return transactionService.getByUserId(id);
+
+        return transactionService.getByUserId(accountService.getAccountsByUserId(id));
     }
 }
--- src/main/java/com/alkemy/wallet/repository/IUserRepository.java
+++ src/main/java/com/alkemy/wallet/repository/IUserRepository.java
@@ -1,4 +1,4 @@
-        package com.alkemy.wallet.repository;
+package com.alkemy.wallet.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;

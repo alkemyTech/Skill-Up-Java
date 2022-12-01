3505481371f20e6a789adf3170ac20dee168c165
AndresTabor <andrestaborda43@gmail.com>
2022-12-01 13:05:33 -0500

    añadiendo AccountServiceTest

--- src/main/java/com/alkemy/wallet/service/AccountService.java
+++ src/main/java/com/alkemy/wallet/service/AccountService.java
@@ -1,6 +1,7 @@
 package com.alkemy.wallet.service;
 
 import com.alkemy.wallet.dto.AccountDto;
+import com.alkemy.wallet.mapper.Mapper;
 import com.alkemy.wallet.model.Account;
 import com.alkemy.wallet.repository.IAccountRepository;
 import com.alkemy.wallet.service.interfaces.IAccountService;
@@ -11,27 +12,35 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
-import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.toList;
+
 
 @Service
 public class AccountService implements IAccountService {
 
-    @Autowired
+
     private IAccountRepository accountRepository;
 
-    @Autowired
+
     private ModelMapper mapper;
+
+    public AccountService(IAccountRepository accountRepository, ModelMapper mapper) {
+        this.accountRepository = accountRepository;
+        this.mapper = mapper;
+    }
 
     @Override
     @Transactional(readOnly = true)
     public List<AccountDto> getAccountsByUserId( Long userId ) throws EmptyResultDataAccessException{
         List<Account> accounts = accountRepository.getAccountsByUser(userId);
+
         if (accounts.isEmpty()){
             throw new EmptyResultDataAccessException("El usuario no posee cuentas",1 );
         }
-        return accounts.stream().map( account ->
-                mapper.map(account, AccountDto.class)
-        ).collect(Collectors.toList());
+        return accounts.stream().map(account ->
+                    mapper.map(account, AccountDto.class)
+        ).toList();
 
     }
 }

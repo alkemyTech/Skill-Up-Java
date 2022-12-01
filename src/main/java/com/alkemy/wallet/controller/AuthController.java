4324a8baeb4c3e36beed2aca5daddfb41aea31b3
AstroCaro <silvagarcesc@gmail.com>
2022-12-01 17:33:36 -0300

    Last changes in register-user-endpoint

--- src/main/java/com/alkemy/wallet/controller/AuthController.java
+++ src/main/java/com/alkemy/wallet/controller/AuthController.java
@@ -1,14 +1,11 @@
 package com.alkemy.wallet.controller;
 
 import com.alkemy.wallet.dto.ResponseUserDto;
-import com.alkemy.wallet.model.User;
-import com.alkemy.wallet.repository.IUserRepository;
 import com.alkemy.wallet.service.CustomUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;

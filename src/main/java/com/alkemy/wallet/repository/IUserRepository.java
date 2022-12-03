e4cfa2790e07c49f22e4de34845b841fe38389ad
contra3000 <contra3000@gmail.com>
2022-12-03 18:31:41 -0300

    findUser in UserController

--- src/main/java/com/alkemy/wallet/repository/IUserRepository.java
+++ src/main/java/com/alkemy/wallet/repository/IUserRepository.java
@@ -1,17 +1,25 @@
 package com.alkemy.wallet.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.alkemy.wallet.model.User;
+
+import java.util.List;
 
 @Repository
 public interface IUserRepository extends JpaRepository<User, Long> {
 
     Boolean existsByEmail(String username);
 
-    User findByEmail(String email);
+    User findOne(String email);
 
     Boolean existsById(long id);
+
+    List<User> findAll();
+
+    Page<User> findAll(Pageable pageable);
 
 }

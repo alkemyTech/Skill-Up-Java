e165c8ad76a6677eae2b149f3264456bfce84943
contra3000 <contra3000@gmail.com>
2022-12-11 22:18:38 -0300

    AccountController Documentation TransactionsController Documentation

--- src/main/java/com/alkemy/wallet/exception/HandlerExceptionAccounts.java
+++ src/main/java/com/alkemy/wallet/exception/HandlerExceptionAccounts.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import javax.validation.ConstraintViolationException;
-
 @Hidden
 @RestControllerAdvice
 public class HandlerExceptionAccounts {
